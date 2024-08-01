46fc22d1ed1006e599b724d40a351e47a07566be
Youssef <youssefkenawy778@gmail.com>
2024-08-01 22:37:02 +0200

    add missing updates

--- src/test/java/Projects/CreateProject.java
+++ src/test/java/Projects/CreateProject.java
@@ -3,6 +3,7 @@ package Projects;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import utilities.RestAssuredUtilities;
+import utilities.Tokens;
 
 import java.io.IOException;
 import java.util.Map;
@@ -10,22 +11,15 @@ import java.util.Map;
 import static org.hamcrest.Matchers.equalTo;
 import static utilities.JsonUtilitiles.getJsonDataAsMap;
 
-public class CreateProject {
+public class CreateProject  extends  RestAssuredUtilities{
 
     @Test
     public void createProject() throws IOException
     {
-        String endpoint = "https://api.dealapp.sa/staging/projects";
-        String adminToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTE2MzhhNjVkYWQxNTNjYTJmMjYwYTQiLCJyb2xlIjoiQURNSU4iLCJzdGF0dXMiOiJBQ1RJVkUiLCJwaG9uZSI6Iis5NjY1MDA4NDQ2NjYiLCJuYW1lIjoi2YXYrdmF2K8g2KfZhNiu2KfZhNiv2YoiLCJpYXQiOjE3MDYwOTUxNDd9.mZyHWKJCPT7rzf3t0NKJuNCYS1NijJ3yBEACILucncQ";
-        Map<String, String> headers = Map.of(
-                "Content-Type", "application/json; charset=utf-8",
-                "accept", "application/json",
-                "Connection", "keep-alive"
-        );
+        String endpoint = "/projects";
         Map<String, Object> requestBody = getJsonDataAsMap("/Projects/CreateProjectData.json");
-        Response response = RestAssuredUtilities.performPost(endpoint, adminToken, requestBody, headers);
+        Response response = RestAssuredUtilities.performPost(endpoint, Tokens.getInstance().getAdminToken(), requestBody, sendHeaders());
         response.then().statusCode(201);  // Asserting the status code
-        response.then().body("name", equalTo("مشروع9"));  // Asserting specific fields in the response body
 
 
     }
--- src/test/java/Projects/UpdateProject.java
+++ src/test/java/Projects/UpdateProject.java
@@ -2,27 +2,22 @@ package Projects;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import utilities.RestAssuredUtilities;
+import utilities.Tokens;
+
 import java.io.IOException;
 import java.util.Map;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+
+import static org.hamcrest.Matchers.*;
 import static utilities.JsonUtilitiles.getJsonDataAsMap;
 
-public class UpdateProject {
+public class UpdateProject  extends RestAssuredUtilities{
 
     @Test
     public  void updateProject() throws IOException {
-        String endpoint = "https://api.dealapp.sa/staging/projects/669cdd0919b1b4793942fd64";
-    String adminToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTE2MzhhNjVkYWQxNTNjYTJmMjYwYTQiLCJyb2xlIjoiQURNSU4iLCJzdGF0dXMiOiJBQ1RJVkUiLCJwaG9uZSI6Iis5NjY1MDA4NDQ2NjYiLCJuYW1lIjoi2YXYrdmF2K8g2KfZhNiu2KfZhNiv2YoiLCJpYXQiOjE3MDYwOTUxNDd9.mZyHWKJCPT7rzf3t0NKJuNCYS1NijJ3yBEACILucncQ";
-        Map<String, String> headers = Map.of(
-                "Content-Type", "application/json; charset=utf-8",
-                "accept", "application/json",
-                "Connection", "keep-alive"
-        );
+        String endpoint = "/projects/669cdd0919b1b4793942fd64";
         Map<String, Object> requestBody = getJsonDataAsMap("/Projects/UpdateProjectData.json");
-        Response response = RestAssuredUtilities.performPatch(endpoint, adminToken, requestBody, headers);
+        Response response = performPatch(endpoint, Tokens.getInstance().getAdminToken(), requestBody, sendHeaders());
         response.then().statusCode(200);  // Asserting the status code
-        response.then().body("name", equalTo("updatedProjectName1"));  // Asserting specific fields in the response body
         response.then().body("updatedAt", notNullValue());
 
     }
