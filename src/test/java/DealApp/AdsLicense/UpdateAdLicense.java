d80b8cbd288b59c644f579d4275f699e9115d26e
Youssef <youssefkenawy778@gmail.com>
2024-10-03 16:00:00 +0300

    Refactor and complete Ad license tests

--- src/test/java/DealApp/AdsLicense/UpdateAdLicense.java
+++ src/test/java/DealApp/AdsLicense/UpdateAdLicense.java
@@ -1,4 +1,64 @@
 package DealApp.AdsLicense;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.RestAssuredUtilities;
+import utilities.Tokens;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Random;
 
-public class UpdateAdLicense {
+public class UpdateAdLicense extends RestAssuredUtilities {
+    public static String generateRandomKsaPhoneNumber() {
+        Random random = new Random();
+        StringBuilder phoneNumber = new StringBuilder("+9665");
+        for (int i = 1; i <= 8; i++) {
+            phoneNumber.append(random.nextInt(9));  // Append a random digit (0-9)
+        }
+        return phoneNumber.toString();
+    }
+
+    @Test(dependsOnMethods = {"DealApp.ADS.CreateAd.createAd", "DealApp.Subscriptions.SubscribeToPackage.createCoupon", "DealApp.AdsLicense.CreateLicense.addLicense", "DealApp.AdsLicense.CreateLicense.upgradeLicense", "DealApp.AdsLicense.CreateLicense.addLicenseForOwner"})
+    public void updateLicenseForOwner() throws IOException {
+        String endpoint = "/ad-license-request/" + CreateLicense.licenseId;
+        String requestBodyJson = new String(Files.readAllBytes(Paths.get("src/test/dealResources/stagingEnv/AdsLisence/addLicenseForOwner.json")), StandardCharsets.UTF_8);
+        // Replace the static adId in the JSON string with the dynamic adId from CreateAd
+        requestBodyJson = requestBodyJson.replace("\"ownerPhone\": \"+966100000001\"", "\"ownerPhone\": \"" + generateRandomKsaPhoneNumber() + "\"");
+        Map<String, Object> requestBody = new ObjectMapper().readValue(requestBodyJson, new TypeReference<Map<String, Object>>() {
+        });
+
+        Response response = performPatch(endpoint, Tokens.getInstance().getReaToken(), requestBody, sendHeaders());
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertNotNull(response.jsonPath().getString("updatedAt"), "Updated At must not be bull");
+    }
+    public void updateLicenseForAttorney() throws IOException {
+        String endpoint = "/ad-license-request/" + CreateLicense.licenseId;
+        String requestBodyJson = new String(Files.readAllBytes(Paths.get("src/test/dealResources/stagingEnv/AdsLisence/addLicenseForAttorney.json")), StandardCharsets.UTF_8);
+        // Replace the static adId in the JSON string with the dynamic adId from CreateAd
+        requestBodyJson = requestBodyJson.replace("\"ownerPhone\": \"+966100000001\"", "\"ownerPhone\": \"" + generateRandomKsaPhoneNumber() + "\"");
+        Map<String, Object> requestBody = new ObjectMapper().readValue(requestBodyJson, new TypeReference<Map<String, Object>>() {
+        });
+
+        Response response = performPatch(endpoint, Tokens.getInstance().getReaToken(), requestBody, sendHeaders());
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertNotNull(response.jsonPath().getString("updatedAt"), "Updated At must not be bull");
+    }
+    public void updateLicenseForEntity() throws IOException {
+        String endpoint = "/ad-license-request/" + CreateLicense.licenseId;
+        String requestBodyJson = new String(Files.readAllBytes(Paths.get("src/test/dealResources/stagingEnv/AdsLisence/addLicenseForEntity.json")), StandardCharsets.UTF_8);
+        // Replace the static adId in the JSON string with the dynamic adId from CreateAd
+        requestBodyJson = requestBodyJson.replace("\"ownerPhone\": \"+966100000001\"", "\"ownerPhone\": \"" + generateRandomKsaPhoneNumber() + "\"");
+        Map<String, Object> requestBody = new ObjectMapper().readValue(requestBodyJson, new TypeReference<Map<String, Object>>() {
+        });
+
+        Response response = performPatch(endpoint, Tokens.getInstance().getReaToken(), requestBody, sendHeaders());
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertNotNull(response.jsonPath().getString("updatedAt"), "Updated At must not be bull");
+    }
+
 }
---
+++ src/test/java/DealApp/Ratings/CreateRating.java
@@ -0,0 +1,8 @@
+package DealApp.Ratings;
+
+import utilities.RestAssuredUtilities;
+
+public class CreateRating extends RestAssuredUtilities
+{
+
+}
